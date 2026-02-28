2dbc536cf6c211e43ccff23fd3e80794b8b2d4a8
Pankaj kumar <psingh18mtr@gmail.com>
2026-02-28 13:51:28 +0530

    patched syntax filtration bug

--- src/orbit/core/signal_analyzer.py
+++ src/orbit/core/signal_analyzer.py
@@ -107,10 +107,10 @@ class SignalAnalyzer(Authenticator):
         try:
             sentiment = self.redis_client.get("market_sentiments")
             if sentiment:
-                if sentiment == 'BULLISH' and signal[0] == "SELL":
+                if sentiment == 'BULLISH' and signal == "SELL":
                     self.send_alerts(data=f"{symbol}", description=f"Positive sentiment, but Sell signal", fields=None)
                     return True
-                elif sentiment == 'BEARISH' and signal[0] == "BUY":
+                elif sentiment == 'BEARISH' and signal == "BUY":
                     self.send_alerts(data=f"{symbol}", description=f"Negative sentiment, but Buy signal", fields=None)
                     return True
         except Exception as e:
