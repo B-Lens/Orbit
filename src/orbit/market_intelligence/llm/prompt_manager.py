0d8f3b6f4f4ded4ffbbf14ca419b7a76cb87c585
Pankaj kumar <psingh18mtr@gmail.com>
2026-05-18 20:26:06 +0530

    fixed env variable

--- src/orbit/market_intelligence/llm/prompt_manager.py
+++ src/orbit/market_intelligence/llm/prompt_manager.py
@@ -230,7 +230,7 @@ class PromptManager:
                 self.langfuse_client = langfuse.Langfuse(
                     public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
                     secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
-                    host=os.getenv("LANGFUSE_HOST", "https://cloud.langfuse.com"),
+                    host=os.getenv("LANGFUSE_BASE_URL", "https://cloud.langfuse.com"),
                 )
                 # Quick connectivity check
                 self.langfuse_client.auth_check()
